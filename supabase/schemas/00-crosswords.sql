9bb516bc65f6a48d01afec5dac1cf34ea84b0d58
thebigthing313 <adriankabigting@gmail.com>
2025-08-09 19:04:34 -0400

    feat: add row level security and policies for crosswords and crossword solutions

--- supabase/schemas/00-crosswords.sql
+++ supabase/schemas/00-crosswords.sql
@@ -29,3 +29,89 @@ create table "public"."crossword_solutions" (
     "created_at" timestamp with time zone not null default now(),
     "updated_at" timestamp with time zone default now()
 );
+
+alter table "public"."crosswords" enable row level security;
+alter table "public"."crossword_solutions" enable row level security;
+
+create policy "Anyone can view public crosswords, owners can view own"
+    on "public"."crosswords"
+    for select
+    to authenticated, anon
+    using (
+        is_public = true
+        or user_id = (SELECT auth.uid())
+    );
+
+create policy "Auth users can insert crosswords"
+    on "public"."crosswords"
+    for insert
+    to authenticated
+    with check (
+        true
+    );
+
+create policy "Auth users can update their own crosswords"
+    on "public"."crosswords"
+    for update
+    to authenticated
+    using (
+        user_id = (SELECT auth.uid())
+    )
+    with check (
+        user_id = (SELECT auth.uid())
+    );
+
+create policy "Auth users can delete their own crosswords"
+    on "public"."crosswords"
+    for delete
+    to authenticated
+    using (
+        user_id = (SELECT auth.uid())
+    );
+
+create policy "Auth users can view own crossword solutions"
+    on "public"."crossword_solutions"
+    for select
+    to authenticated
+    using (
+        crossword_id in (
+            SELECT id
+            FROM public.crosswords
+            WHERE user_id = (SELECT auth.uid()))
+    );
+
+create policy "Auth users can insert crossword solutions"
+    on "public"."crossword_solutions"
+    for insert
+    to authenticated
+    with check (
+        true
+    );
+
+create policy "Auth users can update own crossword solutions"
+    on "public"."crossword_solutions"
+    for update
+    to authenticated
+    using (
+        crossword_id in (
+            SELECT id
+            FROM public.crosswords
+            WHERE user_id = (SELECT auth.uid()))
+    )
+    with check (
+        crossword_id in (
+            SELECT id
+            FROM public.crosswords
+            WHERE user_id = (SELECT auth.uid()))
+    );
+
+create policy "Auth users can delete own crossword solutions"
+    on "public"."crossword_solutions"
+    for delete
+    to authenticated
+    using (
+        crossword_id in (
+            SELECT id
+            FROM public.crosswords
+            WHERE user_id = (SELECT auth.uid()))
+    );
